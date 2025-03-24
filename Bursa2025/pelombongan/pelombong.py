0793ca7c14e2e26bc763a03845cfcad55a3652c1
sithsenz <huang2ming2gui4@gmail.com>
2025-03-24 16:32:57 +0800

    Updated files via upload

--- Bursa2025/pelombongan/pelombong.py
+++ Bursa2025/pelombongan/pelombong.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -45,22 +48,18 @@ def dapatkan_semua_url(laman_screener: str) -> set:
     return semua_url_yang_unik
 
 
-def simpan_laman(url) -> None:
+def simpan_laman(indeks, url, jumlah_url) -> None:
     '''
     Menyimpan sumber halaman web dari URL yang diberikan ke dalam fail HTML.
 
     Fungsi ini mengambil URL, memuatkannya menggunakan pemandu web (driver),
     dan menyimpan sumber halaman yang dimuatkan ke dalam fail HTML dengan nama
-    yang berdasarkan nombor stok yang diekstrak dari URL. Jika berlaku ralat
-    semasa memuatkan atau menyimpan halaman, URL yang bermasalah akan ditambahkan
-    ke dalam senarai 'bermasalah'.
+    yang berdasarkan nombor stok yang diekstrak dari URL.
 
     Args:
         indeks (int): Indeks URL semasa dalam senarai URL yang diproses.
         url (str): URL halaman web yang akan disimpan.
         n (int): Jumlah keseluruhan URL yang akan diproses.
-        bermasalah (list): Senarai untuk menyimpan URL yang bermasalah.
-        pemandu: Objek pemandu web (driver) yang digunakan untuk memuatkan halaman web.
 
     Returns:
         None: Fungsi ini tidak mengembalikan nilai.
@@ -83,3 +82,75 @@ def simpan_laman(url) -> None:
 
     with open(nama_file_laman, "w") as halaman:
         halaman.write(pemandu_chrome.page_source)
+    
+    print(f'   {indeks+1} / {jumlah_url} = {(indeks+1) / jumlah_url:.2%}', end="\r")
+
+
+def dapatkan_nama_saham(sup: BeautifulSoup) -> tuple:
+    '''
+    Mengekstrak nama saham dan kod saham daripada objek BeautifulSoup.
+
+    Fungsi ini mengambil objek BeautifulSoup yang mewakili laman sesawang dan
+    mengambil tajuk halaman untuk mengekstrak nama saham dan kod saham.
+    Tajuk halaman dijangka dalam format "Nama Saham: (Kod Saham)".
+
+    Args:
+        sup (BeautifulSoup): Objek BeautifulSoup yang mengandungi kandungan laman sesawang.
+
+    Returns:
+        tuple: Tuple yang mengandungi nama saham (str) dan kod saham (str).
+
+    Contoh:
+        Jika tajuk halaman ialah "ABC Berhad: (1234)", maka fungsi ini akan mengembalikan
+        ("ABC Berhad", "1234").
+    '''
+    _tajuk: str = sup.find("title")
+    _tajuk = _tajuk.text.strip()
+    nama: str = _tajuk.split(":")[0]
+    _kod: str = _tajuk.split("(")[-1]
+    kod: str = _kod.split(")")[0]
+
+    return nama, kod
+
+
+def dapatkan_harga(sup: BeautifulSoup) -> float:
+    '''
+    Mengekstrak harga saham daripada objek BeautifulSoup.
+
+    Fungsi ini mencari elemen HTML dengan ID "price" dalam objek BeautifulSoup
+    dan mengembalikan harga saham sebagai nilai float.
+
+    Args:
+        sup (BeautifulSoup): Objek BeautifulSoup yang mengandungi kandungan laman sesawang.
+
+    Returns:
+        float: Harga saham yang diekstrak.
+
+    Contoh:
+        Jika elemen dengan ID "price" mengandungi teks "12.34", maka fungsi ini akan
+        mengembalikan 12.34.
+    '''
+    _harga: str = sup.find(attrs={"id": "price"})
+    harga: float = float(_harga.text.strip())
+
+    return harga
+
+
+def dapatkan_data_eps_dps(sup: BeautifulSoup) -> list:
+    df_data: pd.DataFrame = pd.DataFrame(columns=["fy", "eps", "dps"])
+
+    _jadual = sup.find("table", attrs={"class": "financial_reports table table-hover table-sm table-theme"})
+
+    for baris in _jadual.tbody.find_all("tr"):
+        lajur= baris.find_all("td")
+
+        if len(lajur) > 1:
+            f = eval(lajur[7].text.strip()[-4:])
+            e = eval(lajur[0].text.strip())
+            d = eval(lajur[1].text.strip())
+
+            df_data.loc[len(df_data)] = (f, e, d)
+    
+    data = df_data.groupby(["fy"], as_index=False).sum()
+    
+    return data
